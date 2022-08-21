a2adb353d12e091b2b79d4b137b14f5bf328c6fe
Alish Giri <alishgiri.92@gmail.com>
2022-08-21 22:37:27 +0545

    Create data package to connect to the databse

---
+++ database/connect.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"go_admin.com/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	db, err := gorm.Open(mysql.Open("root:password@/go_admin"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database.")
+	}
+
+	DB = db
+	db.AutoMigrate(&models.User{})
+}
