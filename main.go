28e031872e65cb680b9ae10b21d0c79309e78104
Alish Giri <alishgiri.92@gmail.com>
2022-08-21 22:39:52 +0545

    Bring all created funcs to main

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go_admin.com/database"
+	"go_admin.com/routes"
+)
+
+func main() {
+
+	database.Connect()
+
+	app := fiber.New()
+
+	// Important for the front end to receive a cookie
+	// created during login
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":3000")
+
+}
