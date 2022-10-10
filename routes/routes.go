eeb80a075f6605d0d1a0eb88c566004957ce6c09
Alish Giri <alishgiri.92@gmail.com>
2022-10-10 14:32:12 +0545

    Update permission module
    - Pluralize middleware package name
    - Create permission / IsAuthenticated middleware

--- routes/routes.go
+++ routes/routes.go
@@ -3,14 +3,14 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"go_admin.com/controllers"
-	middleware "go_admin.com/middlewares"
+	middlewares "go_admin.com/middlewares"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
 
-	app.Use(middleware.IsAuthenticated)
+	app.Use(middlewares.IsAuthenticated)
 
 	app.Put("/api/users/info", controllers.UpdateInfo)
 	app.Put("/api/users/password", controllers.UpdatePassword)
